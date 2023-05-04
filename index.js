a95c7b0b6ef07797d19c3aee24d26c56c6a5d905
juliantorresmendioroz348@gmail.com <juliantorresmendioroz348@gmail.com>
2023-05-03 22:23:03 -0300

    commit nuevo intel

---
+++ index.js
@@ -0,0 +1,42 @@
+
+/* Muestre una alerta al cargar una página
+Que sea un texto fijo
+Que sean dos variables nombre y apellido concatenadas,
+ mostrando en el mensaje: Nombre: (valor_nombre)  Apellido: (valor_apellido) */
+
+ let button1 = document.querySelector('.button1');
+ let button2 = document.querySelector('.button2');
+ let button3 = document.querySelector('.button3');
+
+ let button1click = button1.addEventListener("click", clickeoBoton);
+
+ let buttonNew =
+
+function clickeoBoton() {
+
+    if(button1click) {
+
+        console.log("tamo chelo")
+    
+    }
+    }
+
+ 
+
+
+
+
+button2.addEventListener("click", clickeoBoton);
+button3.addEventListener("click", clickeoBoton);
+
+    
+
+
+
+//2
+
+// let div1 = document.querySelector('#div1')
+// div1.innerHTML = "Hola soy el div 1"
+
+// let div2 = document.querySelector('.div2')
+// div2.innerHTML = 'Hola, soy el div 2'
